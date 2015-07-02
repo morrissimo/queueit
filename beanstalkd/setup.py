e2cb0ed851f33e14ae899a9e1cce1b078fcd6e6e
Robert Morris <robert.f.morris@gmail.com>
2015-07-01 20:43:46 -0400

    Revert "Revert "added four new "peek" commands""
    This reverts commit 704cfba1849cd05dde35bc41bdc2870cf7a1124f.

--- beanstalkd/setup.py
+++ beanstalkd/setup.py
@@ -13,6 +13,10 @@ setup(
 
     entry_points={
         'console_scripts': [
+            'q-peek = queueit:main',
+            'q-peek-ready = queueit:main',
+            'q-peek-delayed = queueit:main',
+            'q-peek-buried = queueit:main',
             'q-get = queueit:main',
             'q-kick = queueit:main',
             'q-put = queueit:main',
